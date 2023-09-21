626d9da7e91052f02c923884c38c4367815d36c2
MASATAKE KAI <nerotan0806@gmail.com>
2023-09-21 14:42:42 +0900

    bursrファイル作成

---
+++ pages/products/SAMURAI_ENERGY_BURST.js
@@ -0,0 +1,15 @@
+export default function SamuraiEnergyBurst(){
+    return(
+        <>
+            <Head>
+                <title>サムライエナジー｜メイド・イン・ジャパン・エナジードリンク</title>
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="サムライエナジー｜メイド・イン・ジャパン・エナジードリンク" />
+                <meta property="og:site_name" content="サムライエナジー｜メイド・イン・ジャパン・エナジードリンク" />
+                <meta name="twitter:title" content="サムライエナジー｜メイド・イン・ジャパン・エナジードリンク" />
+            </Head>
+
+            <main></main>
+        </>
+    )
+}
